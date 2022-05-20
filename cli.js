067408c231d5ca8a6f2ff835d7b354566fd5153c
trickypr <trickypr@icloud.com>
2022-05-20 17:55:48 +1000

    👽️  Update cli

--- cli.js
+++ cli.js
@@ -1,48 +1,54 @@
 #!/usr/bin/env node
 
-'use strict';
+'use strict'
 
-var program  = require('commander');
-var png2icns = require('./png2icns.js');
+const { program } = require('commander')
+const { convert } = require('./icns')
 
 program
-  .version('0.0.1')
+  .version(require('./package.json').version)
   .arguments('<file>')
-  .usage('<path to .png file> [Options]')
-  .description('Convert a .png file to an .icns file.')
+  .usage('<path to image file> [Options]')
+  .description('Convert an image file to an .icns file.')
   .option(
     '-o, --out <path>',
     '.icns output file name to store as. Default: icon.icns',
-    function (v) {return v;},
+    function (v) {
+      return v
+    },
     'icon.icns'
   )
   .option(
     '-s, --sizes <list>',
     'comma separated list of sizes you want in the icns file.',
-    function (v) {return v.split(',');},
+    function (v) {
+      return v.split(',')
+    },
     [16, 32, 64, 128, 256, 512]
   )
-  .action(function(file, opts) {
-    png2icns({
-      in: file,
-      out: opts.out,
-      sizes: opts.sizes
-    }, function () {
-      console.log('Successfully converted.');
-    });
+  .action(function (file, opts) {
+    convert(
+      {
+        in: file,
+        out: opts.out,
+        sizes: opts.sizes,
+      },
+      function () {
+        console.log('Successfully converted.')
+      }
+    )
   })
-  .on('--help', function(){
-    console.log('  Examples:');
-    console.log('');
-    console.log('    $ png2icns file.png');
-    console.log('    $ png2icns file.png -o file.icns');
-    console.log('    $ png2icns file.png -o file.icns -s 16,32,64,128,256,512');
-    console.log('');
-  });
+  .on('--help', function () {
+    console.log('  Examples:')
+    console.log('')
+    console.log('    $ png2icns file.png')
+    console.log('    $ png2icns file.png -o file.icns')
+    console.log('    $ png2icns file.png -o file.icns -s 16,32,64,128,256,512')
+    console.log('')
+  })
 
+program.parse(process.argv)
 
-program.parse(process.argv);
-
-if(!program.args.length) {
-  program.help();
+if (!program.args.length) {
+  program.help()
 }

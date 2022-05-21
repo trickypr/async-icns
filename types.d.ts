7a9c2575e7f265a86d725cbe6d336f18a10538bc
trickypr <trickypr@icloud.com>
2022-05-21 20:29:55 +1000

    🏷️ Unify types to a single file

--- types.d.ts
+++ types.d.ts
@@ -1,5 +1,44 @@
-// DO NOT EDIT DEFINITION FILES. THEY ARE GENERATED AUTOMATICALLY.
-
 declare module 'async-icns' {
-  /// <reference path="./icns.d.ts" />
+  /**
+   * The config that is used for the primary conversion
+   */
+  declare interface Config {
+    /**
+     * The input file
+     */
+    input: string
+    /**
+     * The output file name
+     */
+    output: string
+    /**
+     * The temporary directory to use
+     */
+    tmpDirectory: string
+    /**
+     * The list of sizes to use
+     */
+    sizes: number[]
+  }
+
+  /**
+   * Resizes an image using SIPs
+   * @param source The source image to be processed
+   * @param out The output file name
+   * @param size The size of the new image
+   */
+  declare function resizeImage(source: string, out: string, size: number): void
+
+  /**
+   *
+   * @param iter The list to iterate through
+   * @returns
+   */
+  declare function forAll<T>(iter: T[]): Promise<Array<U>>
+
+  /**
+   * Convert an image file to a .icns file
+   * @param config The configuration object
+   */
+  declare function convert(config: Config): void
 }
